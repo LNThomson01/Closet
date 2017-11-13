a1cd39ec4c975af4b66b4bd34de251a577a1bfec
Lindsey Thomson <lindseythomson24@gmail.com>
2017-11-13 08:44:42 -0500

    Weather API calls working. TODO display weather data I want

--- Closet/ViewController.swift
+++ Closet/ViewController.swift
@@ -21,12 +21,46 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getWeather()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getWeather()
+    {
+        let session = URLSession.shared
+        let weatherURL = URL(string: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=fb83cf7feb086f5c85003392f4d5a318")!
+        let dataTask = session.dataTask(with: weatherURL) {
+            (data: Data?, response: URLResponse?, error: Error?) in
+            if let error = error {
+                print("Error:\n\(error)")
+            } else {
+                if let data = data {
+                    let dataString = String(data: data, encoding: String.Encoding.utf8)
+                    print("All the weather data:\n\(dataString!)")
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? NSDictionary {
+                        if let mainDictionary = jsonObj!.value(forKey: "main") as? NSDictionary {
+                            if let temperature = mainDictionary.value(forKey: "temp") {
+                                DispatchQueue.main.async {
+                                    self.degreesLabel.text = "Atlanta Temperature: \(temperature)°F"
+                                }
+                            }
+                        } else {
+                            print("Error: unable to find temperature in dictionary")
+                        }
+                    } else {
+                        print("Error: unable to convert json data")
+                    }
+                } else {
+                    print("Error: did not receive data")
+                }
+            }
+        }
+        dataTask.resume()
     }
     
     //MARK: Actions
